99646f1b23490ff30071be181a3cdcea870a0586
BrokenSt0ne <105749252+BrokenSt0ne@users.noreply.github.com>
2023-05-21 14:15:17 +0100

    added source code
    read above

---
+++ PluginInfo.cs
@@ -0,0 +1,12 @@
+﻿namespace GorillaDebug
+{
+    /// <summary>
+    /// This class is used to provide information about your mod to BepInEx.
+    /// </summary>
+    internal class PluginInfo
+    {
+        public const string GUID = "com.brokenstone.gorilladebugger";
+        public const string Name = "GorillaDebugger";
+        public const string Version = "1.0.0";
+    }
+}
